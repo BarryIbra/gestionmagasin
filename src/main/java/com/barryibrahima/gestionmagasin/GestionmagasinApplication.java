4939d6381688430440ff38e47c7c54e4062ed6fc
BarryIbra <bibrahima97@gmail.com>
2024-11-20 22:56:37 +0100

    version 1 avec que les controllers et services

---
+++ src/main/java/com/barryibrahima/gestionmagasin/GestionmagasinApplication.java
@@ -0,0 +1,13 @@
+package com.barryibrahima.gestionmagasin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionmagasinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionmagasinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barryibrahima/gestionmagasin/GestionmagasinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barryibrahima.gestionmagasin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionmagasinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
